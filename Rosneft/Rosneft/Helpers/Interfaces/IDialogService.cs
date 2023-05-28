00b61baa445517fd5f8f3f653f9dd5b4ee13252e
Insaal <100151340+Insaal@users.noreply.github.com>
2023-05-28 21:21:21 +0300

    Изменения в проекте

--- Rosneft/Rosneft/Helpers/Interfaces/IDialogService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace Rosneft.Helpers.Interfaces
-{
-    public interface IDialogService
-    {
-        void ShowMessage(string message, string titlemessage);
-    }
-}
